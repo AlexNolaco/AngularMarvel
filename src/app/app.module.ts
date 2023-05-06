5913f56e1393ed2f6f53e1235256ffea64e33f7d
Alex Nolaço <38504933+AlexNolaco@users.noreply.github.com>
2023-05-06 13:59:36 -0300

    criado interceptor para nas requisições incluir o api key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterModule } from './character/character.module';
 import { AppMaterialModule } from './app.material.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthInterceptor } from './auth-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +21,13 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     CharacterModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
