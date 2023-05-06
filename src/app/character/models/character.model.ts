54ed1e6788889a0a5ce1159e35a3c71cf8e971e0
Alex Nolaço <38504933+AlexNolaco@users.noreply.github.com>
2023-05-06 14:02:05 -0300

    adicionado modelo de character

---
+++ src/app/character/models/character.model.ts
@@ -0,0 +1,10 @@
+import { Thumbnail } from "./thumbnail.model";
+import { Url } from "./url.model";
+
+export interface Character {
+  id: number;
+  name: string;
+  description: string;
+  thumbnail: Thumbnail;
+  urls: Url[];
+}
